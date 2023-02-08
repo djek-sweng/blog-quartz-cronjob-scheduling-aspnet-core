47c19e839c42766d824347e849ea329d83a378c6
djek-sweng <djek@gmx.ch>
2023-02-08 16:31:46 +0100

    NoteController

--- src/CronJobScheduling.WebApi/Program.cs
+++ src/CronJobScheduling.WebApi/Program.cs
@@ -2,10 +2,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddCronJobScheduling();
+builder.Services.AddCronJobSchedulingJobs();
 builder.Services.AddCronJobSchedulingDataStore(
     migrationsAssembly: typeof(ApplicationDbContext).Assembly,
     connectionString: builder.Configuration.GetConnectionString("Npgsql"));
-builder.Services.AddCronJobSchedulingJobs();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle

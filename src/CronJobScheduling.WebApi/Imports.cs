0a21226d0bc1a744a0b206e0b5c0def7565851e4
djek-sweng <djek@gmx.ch>
2023-02-08 11:52:34 +0100

    UseCronJobSchedulingDataStore

--- src/CronJobScheduling.WebApi/Imports.cs
+++ src/CronJobScheduling.WebApi/Imports.cs
@@ -1,2 +1,4 @@
 global using Microsoft.AspNetCore.Mvc;
+global using CronJobScheduling.DataStore.Data;
+global using CronJobScheduling.DataStore.Extensions;
 global using CronJobScheduling.Extensions;
--- src/CronJobScheduling.WebApi/Program.cs
+++ src/CronJobScheduling.WebApi/Program.cs
@@ -2,6 +2,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddCronJobScheduling();
+builder.Services.AddCronJobSchedulingDataStore(
+    migrationsAssembly: typeof(ApplicationDbContext).Assembly,
+    connectionString: builder.Configuration.GetConnectionString("Npgsql"));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -9,6 +12,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseCronJobSchedulingDataStore();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())

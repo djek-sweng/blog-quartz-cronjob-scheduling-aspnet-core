a8ec4e83616cc80efbf6153e10dad2c6c1b32153
djek-sweng <djek@gmx.ch>
2023-02-08 11:27:24 +0100

    DataStore

---
+++ src/CronJobScheduling.DataStore/Imports.cs
@@ -0,0 +1,5 @@
+global using Microsoft.AspNetCore.Builder;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Metadata.Builders;
+global using CronJobScheduling.DataStore.Models;

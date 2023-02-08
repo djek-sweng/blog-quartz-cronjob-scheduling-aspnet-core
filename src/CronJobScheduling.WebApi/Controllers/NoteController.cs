37b9dac8cd1ad0c7082e1c3ffb0a2720fb598e34
djek-sweng <djek@gmx.ch>
2023-02-08 16:31:23 +0100

    NoteController

---
+++ src/CronJobScheduling.WebApi/Controllers/NoteController.cs
@@ -0,0 +1,23 @@
+namespace CronJobScheduling.WebApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class NoteController : ControllerBase
+{
+    private readonly INoteRepository _noteRepository;
+
+    public NoteController(INoteRepository noteRepository)
+    {
+        _noteRepository = noteRepository;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateNoteAsync(CancellationToken cancellationToken)
+    {
+        var note = Note.Create($"Created by {GetType().Name} at {DateTime.UtcNow}");
+
+        await _noteRepository.AddNoteAsync(note, cancellationToken);
+
+        return Ok(note);
+    }
+}

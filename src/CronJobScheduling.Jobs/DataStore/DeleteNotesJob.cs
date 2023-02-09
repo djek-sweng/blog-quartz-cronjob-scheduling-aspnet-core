95fab4d255bada976bedfe8ef73c659ac07e39e2
djek-sweng <djek@gmx.ch>
2023-02-09 09:26:02 +0100

    DeleteNotesJob

--- src/CronJobScheduling.Jobs/DataStore/DeleteNotesJob.cs
+++ src/CronJobScheduling.Jobs/DataStore/DeleteNotesJob.cs
@@ -2,7 +2,7 @@ namespace CronJobScheduling.Jobs.DataStore;
 
 public class DeleteNotesJob : CronJobBase<DeleteNotesJob>
 {
-    public override string Description => "Deletes all notes except the latest 10.";
+    public override string Description => "Deletes all notes except the two latest notes.";
     public override string Group => CronGroupDefaults.User;
     public override string CronExpression => CronExpressionDefaults.EveryMinuteAtSecond0;
 
@@ -15,7 +15,7 @@ public class DeleteNotesJob : CronJobBase<DeleteNotesJob>
 
     protected override async Task ExecuteAsync()
     {
-        var notes = await _noteRepository.GetNotesDescendingAsync(skip: 10);
+        var notes = await _noteRepository.GetNotesDescendingAsync(skip: 2);
 
         await _noteRepository.RemoveNotesAsync(notes);
     }
